d5dd471ddd7959166dba9b064b8034241eb23163
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-02-27 11:16:43 -0300

    Config pino logger and replace all console.log

--- server.ts
+++ server.ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import setupApp from "./src/app";
+import { logger } from "./utils/logger.utils";
 
 const PORT = process.env.PORT || 4000;
 
 setupApp()
   .then((app) =>
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    app.listen(PORT, () => logger.info(`Server running on port ${PORT}`))
   )
   .catch((error: Error) => {
-    console.log(error);
+    logger.error(error);
     process.exit(1);
   });
