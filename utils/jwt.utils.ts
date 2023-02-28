8ea6df33cab684e28889d496f6f31e6dad734ef7
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-02-28 18:39:35 -0300

    Add jwt utils with both create and verify  token

---
+++ utils/jwt.utils.ts
@@ -0,0 +1,8 @@
+import * as jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "shhhhh";
+
+export const createToken = (payload: object | string | Buffer) =>
+  jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+export const verifyToken = (token: string) => jwt.verify(token, JWT_SECRET);
