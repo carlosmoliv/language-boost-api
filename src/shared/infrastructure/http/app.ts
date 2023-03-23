9b8ef7215e47ba26b8f353d59041d9ddf18f48a4
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-03-23 10:07:20 -0300

    feat(documentation): Start using swagger and add course creation to that

--- src/shared/infrastructure/http/app.ts
+++ src/shared/infrastructure/http/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
 
 import api from "./routes";
 import * as database from "../database/mongo";
 import { expressLogger } from "../adapters/logger";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
+
+import swaggerDocs from "../../../swagger.json";
 
 const configureExpress = () => {
   const app: express.Application = express();
@@ -12,6 +15,8 @@ const configureExpress = () => {
   app.use(cors({ origin: "http://localhost:3000" }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
   app.use(expressLogger);
   app.use("/v1", api);
--- src/shared/infrastructure/http/routes/course.routes.ts
+++ src/shared/infrastructure/http/routes/course.routes.ts
@@ -1,14 +1,20 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import { CreateCourseController } from "../../../../modules/courses/infrastructure/http/controllers/CreateCourseController";
 import { CourseRepository } from "../../../../modules/courses/infrastructure/mongo/repositories/CourseRepository";
+import { Role } from "../../../../modules/users/domain/user.enums";
+import { verifyAuthentication } from "../middlewares/verifyAuthentication.middleware";
 
 const courseRepository = new CourseRepository();
 const createCourseController = new CreateCourseController(courseRepository);
 
 const coursesRouter: Router = express.Router();
 
-coursesRouter.post("/", (req: Request, res: Response, next: NextFunction) => {
-  createCourseController.handle(req, res, next);
-});
+coursesRouter.post(
+  "/",
+  verifyAuthentication([Role.admin]),
+  (req: Request, res: Response, next: NextFunction) => {
+    createCourseController.handle(req, res, next);
+  }
+);
 
 export default coursesRouter;
