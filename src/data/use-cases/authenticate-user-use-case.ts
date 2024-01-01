de4c5a7ab8b7642aa89d12369a464176935ac525
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-01 13:45:54 -0300

    refactor: improve code readability in AuthenticateUserUseCase

--- src/data/use-cases/authenticate-user-use-case.ts
+++ src/data/use-cases/authenticate-user-use-case.ts
@@ -4,7 +4,7 @@ import { AccessToken } from '@domain/value-objects'
 import { AuthenticationError } from '@domain/errors'
 
 type Input = { email: string, password: string }
-type Output = { accessToken: string } | null
+type Output = { accessToken: string }
 
 export class AuthenticateUserUseCase {
   constructor (
@@ -15,13 +15,10 @@ export class AuthenticateUserUseCase {
 
   async execute ({ email, password }: Input): Promise<Output> {
     const user = await this.userRepo.findByEmail({ email })
-    if (user !== null) {
-      const passwordMatch = await this.hashComparer.compare({ plainText: password, digest: user.password })
-      if (passwordMatch) {
-        const accessToken = await this.tokenGenerator.generate({ key: user.id, expirationInMs: AccessToken.expirationInMs })
-        return { accessToken }
-      }
-    }
-    throw new AuthenticationError()
+    if (user === null) throw new AuthenticationError()
+    const passwordMatch = await this.hashComparer.compare({ plainText: password, digest: user.password })
+    if (!passwordMatch) throw new AuthenticationError()
+    const accessToken = await this.tokenGenerator.generate({ key: user.id, expirationInMs: AccessToken.expirationInMs })
+    return { accessToken }
   }
 }
