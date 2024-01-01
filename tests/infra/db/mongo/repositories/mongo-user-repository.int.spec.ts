04208753efb717869ec2b4b9d3e2d9e07f83e06c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-01 14:42:35 -0300

    test: ensure MongoUserRepository creates an user

---
+++ tests/infra/db/mongo/repositories/mongo-user-repository.int.spec.ts
@@ -0,0 +1,30 @@
+import { makeFakeUser } from '@tests/factories'
+import { MongoHelper } from '@infra/db/mongo/helpers'
+import { MongoUserRepository } from '@infra/db/mongo/repositories/mongo-user-repository'
+import { env } from '@main/config/env'
+
+describe('MongoUserRepository', () => {
+  let sut: MongoUserRepository
+
+  beforeAll(async () => {
+    await MongoHelper.connect(env.db.mongo.uri)
+    sut = new MongoUserRepository()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('create', () => {
+    it('should create an User', async () => {
+      const data = makeFakeUser()
+
+      await sut.create(data)
+
+      const user = await sut.findByCriteria({ email: data.email })
+      expect(user?.id).toBeTruthy()
+      expect(user?.name).toBe(data.name)
+      expect(user?.email).toBe(data.email)
+    })
+  })
+})
