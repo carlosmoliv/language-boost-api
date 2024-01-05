9371857877da23cb4f9077ea2a9ca041a558a4a9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-05 19:18:37 -0300

    feat: add setupRoutes function to configure API routes

---
+++ src/main/config/routes.ts
@@ -0,0 +1,14 @@
+import { type Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+export const setupRoutes = (app: Express): void => {
+  const router = Router()
+
+  readdirSync(join(__dirname, '../routes'))
+    .filter((file: string) => !file.endsWith('.map'))
+    .map(async (file: string) => {
+      (await import(`../routes/${file}`)).default(router)
+    })
+  app.use('/api', router)
+}
