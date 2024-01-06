4853148f80555726db204f46822c73c9e91674f4
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-06 11:36:10 -0300

    refactor: add response data type for LoginController

--- src/presentation/controllers/auth/login-controller.ts
+++ src/presentation/controllers/auth/login-controller.ts
@@ -3,11 +3,12 @@ import { ok, serverError, unauthorized } from '@presentation/helpers/http'
 import { AuthenticateUserUseCase } from '@data/use-cases'
 
 type HttpRequest = { email: string, password: string }
+type Output = { accessToken: string } | Error
 
 export class LoginController implements Controller {
   constructor (private readonly authenticateUser: AuthenticateUserUseCase) {}
 
-  async handle (request: HttpRequest): Promise<HttpResponse> {
+  async handle (request: HttpRequest): Promise<HttpResponse<Output>> {
     try {
       const { email, password } = request
       const result = await this.authenticateUser.execute({ email, password })
