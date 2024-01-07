5069139fa0ed556f5ad65ea0e14dc7179840325e
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-07 12:15:52 -0300

    refactor: improve ServerError message

--- src/presentation/errors/http.ts
+++ src/presentation/errors/http.ts
@@ -1,6 +1,6 @@
 export class ServerError extends Error {
   constructor (error?: Error) {
-    super('Internal server error. Try again later.')
+    super('Oops! Looks like something went wrong. Try again later.')
     this.name = 'ServerError'
     this.stack = error?.stack
   }
