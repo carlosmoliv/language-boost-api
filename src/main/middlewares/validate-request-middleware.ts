c256420ee4c456f18a3069007d98c2da52523e15
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-09 23:24:26 -0300

    feat: add validateRequest middleware

---
+++ src/main/middlewares/validate-request-middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+import { z } from 'zod'
+
+export const validateRequest = (schema: z.ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+  try {
+    schema.parse(req.body)
+    next()
+  } catch (error) {
+    const errors = error instanceof z.ZodError ? error.issues : undefined
+    res.status(400).json({ error: errors })
+  }
+}
