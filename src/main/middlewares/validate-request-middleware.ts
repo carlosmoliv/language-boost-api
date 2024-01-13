6e9a45e78ccf9eff30135b952ce26d8137f4edce
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-13 10:42:16 -0300

    refactor: improve validateRequest middleware to format Zod errors

--- src/main/middlewares/validate-request-middleware.ts
+++ src/main/middlewares/validate-request-middleware.ts
@@ -6,7 +6,14 @@ export const validateRequest = (schema: z.ZodSchema) => (req: Request, res: Resp
     schema.parse(req.body)
     next()
   } catch (error) {
-    const errors = error instanceof z.ZodError ? error : undefined
+    console.log(error)
+    const errors = error instanceof z.ZodError ? formatZodError(error) : undefined
     res.status(400).json({ errors })
   }
+}
+
+const formatZodError = (error: z.ZodError): Array<{ field: string, message: string }> => {
+  const formattedErrors: Array<{ field: string, message: string }> = []
+  error.issues.forEach((issue) => formattedErrors.push({ field: issue.path.join('.'), message: issue.message }))
+  return formattedErrors
 }
