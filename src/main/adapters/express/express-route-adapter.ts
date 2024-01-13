3af61c4fc67b6a64ed5b431f02f88943a3eec6e2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-13 10:41:23 -0300

    refactor: apply minor code organization in files

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -4,11 +4,7 @@ import { Controller } from '@presentation/interfaces'
 
 export const expressRouteAdapter = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const request = {
-      ...(req.body || {}),
-      ...(req.params || {}),
-      userId: req.userId
-    }
+    const request = { ...req.body, ...req.params, userId: req.userId }
     const { body, statusCode } = await controller.handle(request)
     if (statusCode >= 200 && statusCode <= 299) {
       res.status(statusCode).json(body)
