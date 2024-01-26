cbbce7f067549e00d519648c2c6598cce06ffcd3
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-26 13:39:38 -0300

    feat: use DbTransaction decorator to wrap RegisterController

--- src/main/factories/presentation/controllers/make-register-student-controller.ts
+++ src/main/factories/presentation/controllers/make-register-student-controller.ts
@@ -1,6 +1,10 @@
 import { makeRegisterStudentUseCase } from '@main/factories/application/use-cases'
+import { makeDbTransactionControllerDecorator } from '@main/factories/presentation/decorators'
 import { RegisterStudentController } from '@presentation/controllers/student'
 import { MongoConnection } from '@infra/db/mongo/helpers'
+import { Controller } from '@presentation/interfaces'
 
-export const makeRegisterStudentController = (): RegisterStudentController =>
-  new RegisterStudentController(makeRegisterStudentUseCase(), MongoConnection.getInstance())
+export const makeRegisterStudentController = (): Controller => {
+  const controller = new RegisterStudentController(makeRegisterStudentUseCase(), MongoConnection.getInstance())
+  return makeDbTransactionControllerDecorator(controller)
+}
