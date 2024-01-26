8df90e2a7ec1b99e2fe1f68f94cd983808e9d4d0
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-01-26 13:43:01 -0300

    refactor: remove transaction management from RegisterStudent controller

--- src/main/factories/presentation/controllers/make-register-student-controller.ts
+++ src/main/factories/presentation/controllers/make-register-student-controller.ts
@@ -1,10 +1,9 @@
 import { makeRegisterStudentUseCase } from '@main/factories/application/use-cases'
 import { makeDbTransactionControllerDecorator } from '@main/factories/presentation/decorators'
 import { RegisterStudentController } from '@presentation/controllers/student'
-import { MongoConnection } from '@infra/db/mongo/helpers'
 import { Controller } from '@presentation/interfaces'
 
 export const makeRegisterStudentController = (): Controller => {
-  const controller = new RegisterStudentController(makeRegisterStudentUseCase(), MongoConnection.getInstance())
+  const controller = new RegisterStudentController(makeRegisterStudentUseCase())
   return makeDbTransactionControllerDecorator(controller)
 }
--- src/presentation/controllers/student/register-student-controller.ts
+++ src/presentation/controllers/student/register-student-controller.ts
@@ -1,27 +1,20 @@
 import { EmailAlreadyInUseError } from '@application/use-cases/errors'
 import { RegisterStudentUseCase } from '@application/use-cases/register-user-use-case'
-import { DbTransaction } from '@presentation/contracts'
 import { conflict, noContent, serverError } from '@presentation/helpers'
 import { Controller, HttpResponse } from '@presentation/interfaces'
 
 export class RegisterStudentController implements Controller {
   constructor (
-    private readonly registerStudent: RegisterStudentUseCase,
-    private readonly dbTransaction: DbTransaction
+    private readonly registerStudent: RegisterStudentUseCase
   ) {}
 
   async handle (request: RegisterStudentController.Request): Promise<HttpResponse<RegisterStudentController.Response>> {
-    await this.dbTransaction.openTransaction()
     try {
       const result = await this.registerStudent.execute(request)
       if (result.isLeft() && result.value instanceof EmailAlreadyInUseError) return conflict(result.value)
-      await this.dbTransaction.commitTransaction()
       return noContent()
     } catch (error) {
-      await this.dbTransaction.rollbackTransaction()
       return serverError(error)
-    } finally {
-      await this.dbTransaction.closeTransaction()
     }
   }
 }
