7f21180045985ca0fcaa950957b1f2ec7b4597e8
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-02-09 14:30:21 -0300

    refactor: apply StudentMapper in MongoStudenRepository.findById

--- src/infra/db/mongo/repositories/mongo-student-repository.ts
+++ src/infra/db/mongo/repositories/mongo-student-repository.ts
@@ -1,7 +1,9 @@
+import mongoose from 'mongoose'
+
 import { MongoUserModel, MongoStudentModel } from '@infra/db/mongo/models'
 import { StudentRepository } from '@application/contracts/repositories'
 import { MongoHelper } from '@infra/db/mongo/helpers'
-import mongoose from 'mongoose'
+import { StudentMap } from '@infra/db/mongo/helpers/mappers'
 
 export class MongoStudentRepository implements StudentRepository {
   async create ({ onboarding, id, ...userData }: StudentRepository.CreateInput): Promise<void> {
@@ -14,8 +16,8 @@ export class MongoStudentRepository implements StudentRepository {
     return student && MongoHelper.map(student.toObject())
   }
 
-  async findById ({ id }: StudentRepository.FindByIdInput): Promise<StudentRepository.FindByEmailOutput> {
+  async findById ({ id }: StudentRepository.FindByIdInput): Promise<StudentRepository.FindByIdOutput> {
     const student = await MongoUserModel.findById(new mongoose.Types.ObjectId(id)).populate('student')
-    return student && MongoHelper.map(student.toObject())
+    return student && StudentMap.toDomain(student.toObject())
   }
 }
