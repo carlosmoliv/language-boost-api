793b8dd6877d11fba96a6ef5d0eb5bbf392b489f
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-08 15:24:42 -0300

    refactor: convert StudentMap from static class to a module module structure

--- src/infra/db/mongo/helpers/mappers/student-map.ts
+++ src/infra/db/mongo/helpers/mappers/student-map.ts
@@ -1,8 +1,7 @@
 import { Student, StudentProps } from '@domain/entities'
 
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class StudentMap {
-  public static toDomain (raw: any): Student {
+export const StudentMap = {
+  toDomain (raw: any): Student {
     const props: StudentProps = {
       name: raw.name,
       email: raw.email,
@@ -14,9 +13,9 @@ export class StudentMap {
       onboarding: raw.student.onboarding ?? undefined
     }
     return new Student(props)
-  }
+  },
 
-  public static toPersistance (student: Student): any {
+  toPersistance (student: Student): any {
     return {
       user: {
         _id: student.id,
