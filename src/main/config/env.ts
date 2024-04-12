093d937e246b7e9883fefb70c3369dbf8820c22a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-12 20:47:15 -0300

    feat: set up an isolate test database when running integration tests

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,8 @@
 export const env = {
-  port: process.env.APP_PORT ?? 8080,
+  port: process.env.APP_PORT ?? 8000,
   db: {
     mongo: {
-      uri: process.env.MONGO_URI ?? 'mongodb://localhost:27017/language-boost-api-test'
+      uri: process.env.MONGO_URI ?? 'mongodb://localhost:27017/language-boost-api'
     }
   },
   jwt: {
