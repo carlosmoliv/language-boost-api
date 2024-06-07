646e9040b6e2a3071dfa9922b8e579914969596d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-06-07 08:29:12 -0300

    refactor: remove unnecessary test

--- tests/domain/entities/access-token.unit.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AccessToken } from '../../../src/common'
-
-describe('AccessToken', () => {
-  test('Expiration time of 30 minutes', async () => {
-    expect(AccessToken.expirationInMs).toBe(30 * 60 * 1000)
-  })
-})
